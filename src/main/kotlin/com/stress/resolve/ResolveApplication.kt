83690baea8cfb7d521c4276c70db4b877d61e128
KoorunG <onlyzion10@naver.com>
2024-12-18 01:06:50 +0900

    init project

---
+++ src/main/kotlin/com/stress/resolve/ResolveApplication.kt
@@ -0,0 +1,11 @@
+package com.stress.resolve
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResolveApplication
+
+fun main(args: Array<String>) {
+    runApplication<ResolveApplication>(*args)
+}
