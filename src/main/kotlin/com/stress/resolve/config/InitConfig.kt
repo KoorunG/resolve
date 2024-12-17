4ef5825401309c9be8441d871946d9c31d6f7629
KoorunG <onlyzion10@naver.com>
2024-12-18 01:31:14 +0900

    feat: initConfig 구현
    어플리케이션 구동 시점에 테스트 데이터 입력

---
+++ src/main/kotlin/com/stress/resolve/config/InitConfig.kt
@@ -0,0 +1,24 @@
+package com.stress.resolve.config
+
+import com.stress.resolve.domain.User
+import com.stress.resolve.repository.UserRepository
+import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class InitConfig {
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @PostConstruct
+    fun init() {
+        repeat(5) {
+            userRepository.save(User(
+                name = "테스트유저$it",
+                age = it * 10,
+                description = "테스트유저 $it 입니다."))
+        }
+    }
+}
